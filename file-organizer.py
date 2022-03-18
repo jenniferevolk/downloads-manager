052b61c7d01af0dc8366b030672bc88bbb695d32
Jennifer Volk <jenniferevolk@gmail.com>
2022-03-17 22:56:57 -0700

    Add files via upload
    neatened as per pep-8 standards

--- file-organizer.py
+++ file-organizer.py
@@ -1,29 +1,32 @@
 import os
 import zipfile
+from zipfile import BadZipfile
 import shutil
 import filecmp
 
+
 def unzip(filename):
     destination = os.path.splitext(filename)[0]
+    if os.path.isdir(destination):
+        n = 2
+        while os.path.isdir(f"{destination}({n})"):
+            n = n + 1
+        destination = f"{destination}({n})"
+    os.mkdir(destination)
     try:
-        if os.path.isdir(destination):
-            n=2
-            while os.path.isdir(f"{destination}({n})"):
-                n=n+1
-            destination=f"{destination}({n})"
-        os.mkdir(destination)
         with zipfile.ZipFile(filename, 'r') as zipObj:
             zipObj.extractall(destination)
-    except:
-        print(f"[-] ERROR unzipping {filename} to {destination}")
+    except BadZipfile:
+        print(f"[-] ERROR bad zipfile {filename}")
     else:
         os.remove(filename)
         print(f"[+] Unzipped {filename} to {destination}")
 
+
 def move(filename):
-    extension=os.path.splitext(filename)[1][1:]
+    extension = os.path.splitext(filename)[1][1:]
     if extension in file_type:
-        destination=file_type[extension]
+        destination = file_type[extension]
         if not os.path.isdir(destination):
             os.mkdir(destination)
             print(f"[+] created {destination} folder")
@@ -32,55 +35,52 @@ def move(filename):
             shutil.move(filename, destination)
             print(f"[+] moved {filename} to {destination}")
         else:
-            duplicate=filecmp.cmp(filename, f"{destination}/{filename}", shallow=False)
+            duplicate = filecmp.cmp(filename, f"{destination}/{filename}", shallow=False)
             if duplicate:
                 print(f"[+] already have {filename}")
                 os.remove(filename)
             else:
                 n = 2
-                file=os.path.splitext(filename)[0]
+                name = os.path.splitext(filename)[0]
                 while os.path.isfile(f"{destination}/{file}({n}).{extension}"):
                     n = n + 1
-                destination = f"{destination}/{file}({n}).{extension}"
+                destination = f"{destination}/{name}({n}).{extension}"
                 shutil.move(filename, destination)
                 print(f"[+] moved {filename} to {destination}")
     else:
         print(f"[-]unknown file type {extension} for {filename}")
 
 
-
 def filelist(path):
     objects = os.listdir(path)
-    files = []
+    list_of_files = []
     for obj in objects:
         if os.path.isfile(obj):
-            files.append(obj)
-    return sorted(files)
+            list_of_files.append(obj)
+    return sorted(list_of_files)
 
-data={
+
+data = {
     "Bins": ["exe"],
-    "Docs": ["pdf","doc","docx","txt"],
+    "Docs": ["pdf", "doc", "docx", "txt"],
     "Gifs": ["gif"],
-    "Pics":["jpg","png"],
+    "Pics": ["jpg", "png"],
     "3D": ["fbx"],
-    "Movies":["mp4"]
+    "Movies": ["mp4"]
 }
 
-#create file type index
-folders=list(data.keys())
-file_type={}
+# create file type index
+folders = list(data.keys())
+file_type = {}
 for folder in folders:
-    for extension in data[folder]:
-        file_type[extension]=folder
+    for filetype in data[folder]:
+        file_type[filetype] = folder
 
-#get list of files
-path=str(os.getcwd())
-files=filelist(path)
+# get list of files
+files = filelist(os.getcwd())
 
 for file in files:
-    extension = os.path.splitext(file)[1][1:]
-    if extension=="zip":
+    if file[-4] == ".zip":
         unzip(file)
     else:
         move(file)
-
